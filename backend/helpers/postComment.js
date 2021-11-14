2965e65e35302b2e15199ffd9a3d4730dc5b3a7a
Basel Alanazi <alanaziibasel@gmail.com>
2021-11-14 11:29:06 +0300

    Add comments helper,controller and routes

---
+++ backend/helpers/postComment.js
@@ -0,0 +1,42 @@
+const dbClient = require("../util/database");
+
+
+async function comment (method,payload){
+    try {
+        await dbClient.connect();
+        const database = dbClient.db("project_db");
+        const comments = database.collection("posts_comments");
+        let query = {}
+        let comment = null
+
+        switch (method) {
+            case "GET_COMMENT":
+                comment = await comments.find( payload.query );  
+                return comment ? comment : "Error";
+
+            case "CREATE_COMMENT":
+                comment = await comments.insertOne(
+                    {
+                        user: payload.user, 
+                        post: payload.post,
+                        comment : payload.content,
+                        date: new Date()
+                    }
+                )
+                return comment ? comment : "Error";
+
+            case "DELETE_COMMENT":
+                   comment = await comments.deleteOne({_id: payload.id})
+                   return comment === 1 ? "Deleted" : "Error";
+
+            default:
+                return null ;
+        }
+
+
+    } finally {
+        await dbClient.close();
+    }
+}
+
+module.exports = comment;
