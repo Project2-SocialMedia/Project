31b801e134da587febf5606fbca66bc4d71caac7
Nouraalqarni <noura.alqarni.ruh.js@tuwaiq.edu.sa>
2021-11-14 11:29:08 +0300

    create like method in helper

---
+++ backend/helpers/postLike.js
@@ -0,0 +1,39 @@
+const dbClient = require("../util/database");
+
+
+async function likePost (method,payload){
+    try {
+        await dbClient.connect();
+        const database = dbClient.db("project_db");
+        const postLikes = database.collection("posts_likes");
+
+
+        switch (method) {
+            case "GET_LIKE":
+                postLike = await postLikes.find( payload.query );  
+                return postLike ? postLike : "Error";
+
+            case "CREATE_LIKE":
+                postLike = await postLikes.insertOne(
+                    {
+                        user: payload.user, 
+                        post: payload.post
+                    }
+                )
+                return postLike ? postLike : "Error";
+
+            case "DELETE_LIKE":
+                   postLike = await postLikes.deleteOne({_id: payload.id})
+                   return postLike === 1 ? "Deleted" : "Error";
+
+            default:
+                return null ;
+        }
+
+
+    } finally {
+        await dbClient.close();
+    }
+}
+
+module.exports = likePost;
