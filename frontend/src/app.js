1206e11b2056ac226bc92f982593171b7e430506
Basel Alanazi <alanaziibasel@gmail.com>
2021-11-16 11:10:15 +0300

    Edit profile

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -15,7 +15,6 @@ export default function App (){
 			isAuthorized: state.authenticationReducer.userId,
 		};
 	});
-    const [userId, setUserId ] = useState ();
 
     useEffect(() => {
         auth.getUserId(localStorage.getItem("token")).then ( (id) => {
